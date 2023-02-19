23cb73a62464b41e4ec9ca3d6f3238376b1e3892
unknown <hvdeepali@gmail.com>
2023-02-19 18:38:03 +0530

    added disabled feature

--- src/pages/Procurement/AddProcurement.js
+++ src/pages/Procurement/AddProcurement.js
@@ -45,6 +45,7 @@ const AddProcurement = () => {
     errorFields: [],
     isNameInKannada: false,
     addProcurementError: [],
+    submitDisabled: false
   };
 
   const navigate = useNavigate();
@@ -120,11 +121,18 @@ const AddProcurement = () => {
         name: ele.label,
       };
     });
+    setState((prev)=>{
+      return{
+        ...prev,
+        submitDisabled: true
+      }
+    })
     if (state.addPlantName?.__isNew__) {
       body.nameInKannada = state.addPlantKannada;
       body.nameInEnglish = state.addPlantName.label;
 
       const res = await createProcurements({ body });
+
       if (res.error) {
         toast.error("Unable to Add...");
         setState((prev) => {
@@ -133,6 +141,7 @@ const AddProcurement = () => {
             addProcurementError: _.isArray(res.error?.data)
               ? res.error?.data
               : [res.error?.data?.error],
+              submitDisabled:false
           };
         });
       } else {
@@ -153,7 +162,7 @@ const AddProcurement = () => {
     } else {
       const id = state.addPlantName?.value;
       const res = await updateProcurements({ body, id });
-      console.log(res, isErrorUpdate, isSuccessUpdate);
+
       if (res.error) {
         setState((prev) => {
           return {
@@ -161,6 +170,7 @@ const AddProcurement = () => {
             addProcurementError: _.isArray(res.error?.data)
               ? res.error?.data
               : [res.error?.data?.error],
+            submitDisabled:false
           };
         });
       } else {
@@ -294,7 +304,8 @@ const AddProcurement = () => {
                 isEmpty(state.addVendorName) ||
                 isEmpty(state.totalAmount) ||
                 isEmpty(state.totalQuantity)||
-                state.errorFields.length > 0
+                state.errorFields.length > 0 ||
+                state.submitDisabled
 
               }
               type="primary"
