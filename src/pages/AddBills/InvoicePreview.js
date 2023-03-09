4cdcf0ef5c8a272d1952a7a1d166d8aa64bfe94f
ashu.kharbanda <ashu_kharbanda@outlook.com>
2023-03-09 22:00:19 +0530

    changes for invoice genration

---
+++ src/pages/AddBills/InvoicePreview.js
@@ -0,0 +1,168 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Modal } from '@mantine/core'
+import styles from "./AddBills.module.css";
+import ScrollTable from '../../components/Table/ScrollTable';
+import { Button } from '../../components';
+import ReactToPrint from 'react-to-print';
+
+export const InvoicePreview = ({ showPreview, onClose, clientDetails, cartData, children, cartResponse, invoiceNumber, setInvoiceNumber }) => {
+
+  const [cartList, setCartList] = useState();
+  const [invoiceHeader, setInvoiceHeader] = useState([]);
+  // const [showMRP, setShowMRP] = useState(false);
+  const componentRef = useRef();
+
+  const invoiceHeaderWithMRP = [
+    { value: "S. No.", width: '10%' },
+    { value: "Item Purchased", width: '40%' },
+    { value: "MRP", width: '10%' },
+    { value: "Rate", width: '10%' },
+    { value: "Quantity", width: '10%' },
+    { value: "Sub Total", width: '20%' },
+  ]
+
+  const invoiceHeaderWithOutMRP = [
+    { value: "S. No.", width: '10%' },
+    { value: "Item Purchased", width: '40%' },
+    { value: "Rate", width: '15%' },
+    { value: "Quantity", width: '15%' },
+    { value: "Sub Total", width: '20%' },
+  ]
+
+  useEffect(() => {
+
+    const invoiceNum = Math.floor(10000000 + Math.random() * 90000000);
+    setInvoiceNumber(invoiceNum)
+    let newCartList = [];
+    let showMRPTemp = false;
+
+    for (let index = 0; index < cartData.length; index++) {
+      if (cartData[index].mrp !== cartData[index].price) {
+        showMRPTemp = true;
+        break;
+      }
+    }
+
+    if (showMRPTemp) {
+      setInvoiceHeader(invoiceHeaderWithOutMRP)
+    } else {
+      setInvoiceHeader(invoiceHeaderWithMRP)
+    }
+
+    cartData.forEach((el, index) => {
+      let val = []
+      val.push({ value: index + 1 })
+      val.push({ value: el.procurementLabel })
+      if (!showMRPTemp) {
+        val.push({ value: el.mrp })
+      }
+      val.push({ value: el.price })
+      val.push({ value: el.quantity })
+      val.push({ value: el.price * el.quantity })
+      newCartList.push(val)
+    });
+    setCartList(newCartList)
+    // setShowMRP(showRateMRP)
+  }, [cartData])
+
+  return (
+    <Modal opened={showPreview} onClose={onClose} centered size="auto" closeOnClickOutside={false} closeOnEscape={true}>
+      <div className={styles.printButton}>
+        <ReactToPrint
+          trigger={() => <Button
+            type="primary"
+            title="Print"
+            buttonType="submit"
+          />}
+          content={() => componentRef.current}
+        />
+      </div>
+      <div className={styles.modalContent} id="modal-print-section" ref={componentRef}>
+        <div className={styles.headerWrapperModal}>
+          <h1 className={styles.headerModal}>Bill Invoice</h1>
+        </div>
+
+        <div className={styles.clientDetailsWrapper}>
+          <div className={styles.companyDetails}>
+            <div>Sold By</div>
+            <div className={styles.addressDetails}>
+              <b>Noni Nursery</b>
+              <br></br>
+              AirVilla, Airport Road
+              <br></br>
+              Shivmogga 577452
+            </div>
+
+            <div className={styles.lableValueDetails}>
+              <div className={styles.label}>PAN NO:</div>
+              <div className={styles.value}>GBKPS2231C</div>
+            </div>
+
+            <div className={styles.lableValueDetails}>
+              <div className={styles.label}>GST NO:</div>
+              <div className={styles.value}>08GBKPS2231C2ZP</div>
+            </div>
+          </div>
+
+          <div className={styles.clientDetails}>
+            <div className={styles.lableValueDetails}>
+              <div className={styles.label}>Invoice Date:</div>
+              <div className={styles.value}>{formatDate(new Date())}</div>
+            </div>
+
+            <div className={styles.lableValueDetails}>
+              <div className={styles.label}>Invoice Number:</div>
+              <div className={styles.value}>{invoiceNumber}</div>
+            </div>
+
+            <div className={styles.billedTo}>
+              Billed To
+              <br></br>
+              <b>{clientDetails?.name}</b>
+              <br></br>
+              {clientDetails?.phoneNumber}
+              <br></br>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          {invoiceHeader && invoiceHeader.length > 0 && <ScrollTable thead={invoiceHeader} tbody={cartList} />}
+        </div>
+
+        <div className={styles.invoiceSummary}>
+          <div className={styles.lableValueDetails}>
+            <div className={styles.label}>Billed By:</div>
+            <div className={styles.value}>Uday Kiran</div>
+          </div>
+
+          <div className={styles.dicountDetails}>
+            <div className={styles.lableValueDetails}>
+              {cartResponse.discount + cartResponse.roundOff > 0 && <div className={styles.label}>Discount Price: </div>}
+              <div className={styles.label}>Total Price: </div>
+            </div>
+
+            <div className={styles.lableValueDetails}>
+              {cartResponse.discount + cartResponse.roundOff > 0 && <div className={styles.discountValue}><b>&#x20B9;{cartResponse.discount + cartResponse.roundOff}</b></div>}
+              <div className={styles.discountValue}><b>&#x20B9;{cartResponse.totalPrice}</b></div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className={styles.thankYouNote}>Make sure to collect money before submitting!</div>
+      <div className={styles.modalAction}>{children}</div>
+    </Modal>
+  )
+}
+
+export const formatDate = (date) => {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  const strTime = hours + ':' + minutes + ' ' + ampm;
+  return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + " " + strTime;
+}
