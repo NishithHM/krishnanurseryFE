a8e6aa14fb32bd150b2580066d01b99e69930772
NishithHM <nishithhm@Apples-MacBook-Pro-2.local>
2023-04-12 21:11:04 +0530

    fixed

--- src/pages/Bills/Bills.js
+++ src/pages/Bills/Bills.js
@@ -160,7 +160,7 @@ const Bills = () => {
         {
             value: "Date",
             isSortable: true,
-            sortBy: "updatedAt",
+            sortBy: "createdAt",
         },
 
         {
