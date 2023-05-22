3b137ed16fc5c4e5f371dea76511c4bb1ad8b03c
NishithHM <nhm987@gmail.com>
2023-05-22 14:28:59 +0530

    fixed

--- src/pages/RequestOrder/RequestOrder.js
+++ src/pages/RequestOrder/RequestOrder.js
@@ -95,7 +95,7 @@ const RequestOrder = () => {
           <h1 className={styles.header}>Request Order</h1>
           <div className={styles.innerWrapper}>
             <Dropdown
-              url="/api/procurements/getAll?isList=true"
+              url="/api/procurements/getAll?isList=true&isAll=true"
               id="addPlantName"
               apiDataPath={{ label: "names.en.name", value: "_id" }}
               title="Plant Name"
