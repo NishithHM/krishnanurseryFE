85f07bc016f595631b11606f8d015f0cf4f98597
vamsiGangula <vamsigangula9133@gmail.com>
2024-01-31 19:13:21 +0530

    all css changes are done

---
+++ src/icons/LossIcon.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 -960 960 960"
+      fill="#FF2727"
+    >
+      <path d="M480-132L234-378l19-19 213 213v-645h28v644l213-213 19 20-246 246z"></path>
+    </svg>
+  );
+}
+
+export default Icon;
