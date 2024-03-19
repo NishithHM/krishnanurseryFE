5f144fe59e89c4b26aace8944040973172bdb358
NishithHM <nhm987@gmail.com>
2024-03-19 10:18:08 +0530

    fixed

--- src/pages/OrderManagement/orderMgmt.js
+++ src/pages/OrderManagement/orderMgmt.js
@@ -213,7 +213,7 @@ const OrderMgmt = () => {
         });
         setLoading(false);
       };
-
+      console.log(loading, 'loading')
       const handlePlantImageRemove = (index) => {
         setPlantImages((prev) => {
           let updated = [...prev];
