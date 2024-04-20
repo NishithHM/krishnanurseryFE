aa025bf15d06b45cf5624b2e3a6c4ba28018c23f
hegade <subramanyashegade@gmail.com>
2024-04-20 22:26:32 +0530

    chore: added downloadFile helper

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,14 +1,31 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userActions } from "../store/slices/user.slice";
+import axios from "axios";
 
-export const baseQueryWithAuth =async (args, api, extraOptions)=>{
-    const baseQuery = fetchBaseQuery({...extraOptions});
-    let result = await baseQuery(args, api, extraOptions)
-    if(result?.error?.status === 401){
-        api.dispatch(userActions.logout())
+export const baseQueryWithAuth = async (args, api, extraOptions) => {
+  const baseQuery = fetchBaseQuery({ ...extraOptions });
+  let result = await baseQuery(args, api, extraOptions);
+  if (result?.error?.status === 401) {
+    api.dispatch(userActions.logout());
+  }
+  if (result?.error?.status > 499) {
+    api.dispatch(userActions.globalError());
+  }
+  return result;
+};
+
+/*
+ * Function to download file
+ * @param {String} fileName
+ * @returns {Promise}
+ */
+export const downloadFile = async (url) => {
+  const res = axios.get(
+    `${process.env.REACT_APP_BASE_URL}/api/download?path=${url}`,
+    {
+      headers: {
+        Authorization: sessionStorage.getItem("authToken"),
+      },
     }
-    if(result?.error?.status > 499){
-        api.dispatch(userActions.globalError())
-    }
-    return result;
-}
+  );
+};
